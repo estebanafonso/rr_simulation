6ea60e6a0b4f91400c1d286a5bf35de25eec1685
estebanafonso <esteban.afonso@gmail.com>
2014-06-02 12:49:37 -0400

    add recrew.py

--- recrew.py
+++ recrew.py
@@ -1,8 +1,7 @@
 import simpy
-import random
 
 class Train(object):
-    def __init__(self, env, name, max_recrew_period,MTTF):
+    def __init__(self, env, name, max_recrew_period):
         self.env = env
         self.name = name
         self.max_recrew_period = max_recrew_period
@@ -26,7 +25,7 @@ class Train(object):
     def check_for_recrew(self, next_railway):
         self.time_since_last_recrew = env.now - self.time_of_last_recrew
         print "Time since last recrew for %s: %.2f" % (self.name,self.time_since_last_recrew)
-        print "Estimated crew time at next recrew point: %.2f" % (self.time_since_last_recrew + next_railway.get_distance())
+        print "Estimated crew time at next recrew point: %i" % (self.time_since_last_recrew + next_railway.get_distance())
         if self.time_since_last_recrew + next_railway.get_distance() > self.max_recrew_period:
             yield env.process(self.recrew())
             
@@ -39,8 +38,6 @@ class Train(object):
         self.time_since_last_recrew = 0.0
         self.no_of_recrews += 1
         print "%s Cumulative Recrews: %i " % (self.name,self.no_of_recrews)
-    
-        
         
         
     
@@ -61,7 +58,6 @@ class Railway(object):
 print('Railroad Simulation \n')
 
 MAX_RECREW_PERIOD = 10
-MEAN_TIME_TO_FAILURE = 15
 
 env = simpy.Environment()
 
@@ -71,39 +67,23 @@ railway_ns = Railway(env, "track ns", 8)
 railway_ne = Railway(env, "track ne", 2)
 railway_se = Railway(env, "track se", 2)
 
-train_west = Train(env, "West Train", MAX_RECREW_PERIOD,MEAN_TIME_TO_FAILURE)
+train_west = Train(env, "West Train", MAX_RECREW_PERIOD)
 train_east = Train(env, "East Train", MAX_RECREW_PERIOD)
 
-class Setup(object)
-
-    def __init__(
-
-    def train_west_path(env):
-        while True:
-            yield env.process(train_west.ride(railway_nw))
-            yield env.process(train_west.check_for_recrew(railway_ns))
-            yield env.process(train_west.ride(railway_ns))
-            yield env.process(train_west.check_for_recrew(railway_sw))
-            yield env.process(train_west.ride(railway_sw))
-            yield env.process(train_west.check_for_recrew(railway_nw))
-            
-    def train_east_path(env):
-        while True:
-            yield env.process(train_east.ride(railway_ne))
-            yield env.process(train_east.check_for_recrew(railway_se))
-            yield env.process(train_east.ride(railway_se))
-            yield env.process(train_east.check_for_recrew(railway_ns))
-            yield env.process(train_east.ride(railway_ns))
-            yield env.process(train_east.check_for_recrew(railway_ne))
-            
-    def time_to_failure(self):
-            return random.expovariate(1/self.MTTF)
-            
-    def breakdown(self):
-            while True:
-                yield self.env.timeout(self.time_to_failure())
-                if not self.broken:
-                    # breaking machine
+def train_west_path(env):
+    while True:
+        yield env.process(train_west.ride(railway_nw))
+        yield env.process(train_west.check_for_recrew(railway_ns))
+        yield env.process(train_west.ride(railway_ns))
+        yield env.process(train_west.check_for_recrew(railway_sw))
+        yield env.process(train_west.ride(railway_sw))
+        yield env.process(train_west.check_for_recrew(railway_nw))
+        
+def train_east_path(env):
+    while True:
+        yield env.process(train_east.ride(railway_ne))
+        yield env.process(train_east.ride(railway_se))
+        yield env.process(train_east.ride(railway_ns))
         
 env.process(train_west_path(env))
 #env.process(train_east_path(env))
